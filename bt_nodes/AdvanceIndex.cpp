7e68b967f066e17bf9950240d0b846b02d57dbfd
jaeho <woghjaeho1@gmail.com>
2025-06-29 20:14:47 +0900

    Implemented AdvanceIndex Node

--- bt_nodes/AdvanceIndex.cpp
+++ bt_nodes/AdvanceIndex.cpp
@@ -1,18 +1,42 @@
 #include "turtlebot4_factory_inspection/bt_nodes/AdvanceIndex.hpp"
+#include <rclcpp/rclcpp.hpp>
 
 namespace turtlebot4_factory_inspection::bt_nodes {
 
 AdvanceIndex::AdvanceIndex(const std::string& name, const BT::NodeConfiguration& config)
-: BT::SyncActionNode(name, config) {}
+: BT::SyncActionNode(name, config)
+{
+  node_ = rclcpp::Node::make_shared("advance_index_bt_node");
+}
 
 BT::PortsList AdvanceIndex::providedPorts()
 {
-  return {};
+  return {
+    BT::InputPort<int>("current_index"),
+    BT::OutputPort<int>("updated_index")
+  };
 }
 
 BT::NodeStatus AdvanceIndex::tick()
 {
+  int index;
+  if (!getInput("current_index", index))
+  {
+    RCLCPP_ERROR(node_->get_logger(), "[AdvanceIndex] ❌ Missing input [current_index]");
+    return BT::NodeStatus::FAILURE;
+  }
+
+  index += 1;
+
+  if (!setOutput("updated_index", index))
+  {
+    RCLCPP_ERROR(node_->get_logger(), "[AdvanceIndex] ❌ Failed to set output [updated_index]");
+    return BT::NodeStatus::FAILURE;
+  }
+
+  RCLCPP_INFO(node_->get_logger(), "[AdvanceIndex] ✅ Advanced index to: %d", index);
+
   return BT::NodeStatus::SUCCESS;
 }
 
-}  // namespace turtlebot4_factory_inspection
+}  // namespace turtlebot4_factory_inspection::bt_nodes
