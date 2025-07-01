caae0d692ede75efade6b595fd1b3264d4dccc64
jaeho <woghjaeho1@gmail.com>
2025-07-02 00:14:55 +0900

    Implemented load_waypoints

--- include/turtlebot4_factory_inspection/utils/load_waypoints.hpp
+++ include/turtlebot4_factory_inspection/utils/load_waypoints.hpp
@@ -5,11 +5,11 @@
 #include <string>
 #include <geometry_msgs/msg/pose_stamped.hpp>
 
-namespace turtlebot4_factory_inspection::bt_nodes {
+namespace turtlebot4_factory_inspection {
 
 std::pair<std::vector<geometry_msgs::msg::PoseStamped>, std::vector<double>>
 load_waypoints_from_yaml(const std::string& file_path);
 
-}  
+}  // namespace turtlebot4_factory_inspection
 
-#endif  
+#endif
