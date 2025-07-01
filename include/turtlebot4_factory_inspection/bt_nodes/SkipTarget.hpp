822f6b360b2ea7335b82e65213d45cb6021c6f5f
jaeho <woghjaeho1@gmail.com>
2025-07-02 00:13:14 +0900

    SkipTarget Node

--- include/turtlebot4_factory_inspection/bt_nodes/SkipTarget.hpp
+++ include/turtlebot4_factory_inspection/bt_nodes/SkipTarget.hpp
@@ -1,18 +1,16 @@
-#ifndef TURTLEBOT4_FACTORY_INSPECTION_SKIP_TARGET_HPP_
-#define TURTLEBOT4_FACTORY_INSPECTION_SKIP_TARGET_HPP_
+#ifndef SkipTarget_HPP_
+#define SkipTarget_HPP_
 
 #include <behaviortree_cpp_v3/action_node.h>
-
-namespace turtlebot4_factory_inspection::bt_nodes {
 
 class SkipTarget : public BT::SyncActionNode
 {
 public:
   SkipTarget(const std::string& name, const BT::NodeConfiguration& config);
+
   static BT::PortsList providedPorts();
+
   BT::NodeStatus tick() override;
 };
 
-}  // namespace turtlebot4_factory_inspection
-
-#endif  // TURTLEBOT4_FACTORY_INSPECTION_SKIP_TARGET_HPP_
+#endif  // SkipTarget_HPP_
